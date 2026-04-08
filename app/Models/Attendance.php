a5da5083f6847f51b05e775293e71859bf83bc03
Jan Kobe Ignacio <jan.kobe.ignacio@ntt.com.ph>
2026-04-08 15:32:39 +0800

    feat: attendances table migration and some model content changes

--- app/Models/Attendance.php
+++ app/Models/Attendance.php
@@ -5,18 +5,33 @@ namespace App\Models;
 use App\Traits\HasFilterScope;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Attendance extends Model
 {
     use HasUuids, HasFilterScope;
 
     protected $fillable = [
-        'name',
-        'description',
+        'user_id',
+        'date',
+        'time_in',
+        'time_in_notes',
+        'time_out',
+        'time_out_notes',
+        'ip_address'
     ];
 
     protected array $filterable = [
-        'name',
-        'description',
+        'date',
+        'time_in_notes',
+        'time_out_notes',
+        'user.first_name',
+        'user.last_name',
+        'user.email',
     ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
---
+++ database/migrations/2026_04_08_072150_create_attendances_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('attendances', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->foreignUuid('user_id')->constrained()->cascadeOnDelete();
+            $table->date('date');
+            $table->timestamp('time_in')->nullable();
+            $table->string('time_in_notes')->nullable();
+            $table->timestamp('time_out')->nullable();
+            $table->string('time_out_notes')->nullable();
+            $table->string('ip_address')->nullable();
+            $table->timestamps();
+
+            $table->index(['user_id', 'date']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('attendances');
+    }
+};
